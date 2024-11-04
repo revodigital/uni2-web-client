507d31a728bdd5b9b1d3c5754aa196651725d9e5
Diego Cancedda <diego.cancedda.dc@gmail.com>
2024-11-04 12:04:55 +0100

    :bug: fix: Aggiorna alcune configurazioni di build per risolvere alcuni errori

--- src/index.tsx
+++ src/index.tsx
@@ -6,43 +6,57 @@ import React from 'react'
 import { render } from 'react-dom'
 
 // ==============================|| REACT DOM RENDER  ||============================== //
+;
 
-const mapRender = (
-	accessToken: string,
-	target: HTMLElement,
-	options?: {
-		defaultView?: {
-			longitude: number
-			latitude: number
-			zoom: number
+(function (global) {
+	// Il tuo codice esistente
+
+	const mapRender = (
+		accessToken: string,
+		target: HTMLElement,
+		options?: {
+			defaultView?: {
+				longitude: number
+				latitude: number
+				zoom: number
+			}
 		}
+	) => {
+		const container = document.createElement('div')
+		container.setAttribute('id', `MapContainer${target.id}`)
+
+		target.getElementsByClassName('ChoiceStructure')[0].appendChild(container)
+
+		const directionContainer = target.querySelectorAll('[role*=presentation]')[0] as HTMLElement
+
+		const inputHTMLElements = directionContainer.getElementsByTagName('input')
+		const labelHTMLElements = directionContainer.getElementsByTagName('label')
+
+		const inputHtmlArray = [...labelHTMLElements].map((item, index) => ({
+			label: item.children[0].textContent ? item.children[0].textContent : '',
+			htmlElement: inputHTMLElements[index],
+			elementIndex: index
+		}))
+
+		render(
+			<React.StrictMode>
+				<ThemeCustomization>
+					<MapContainer mapBoxToken={accessToken} defaultView={options?.defaultView} inputHtmlArray={inputHtmlArray} />
+				</ThemeCustomization>
+			</React.StrictMode>,
+			document.getElementById(`MapContainer${target.id}`)
+		)
+		directionContainer.style.display = 'none'
 	}
-) => {
-	const container = document.createElement('div')
-	container.setAttribute('id', `MapContainer${target.id}`)
 
-	target.getElementsByClassName('ChoiceStructure')[0].appendChild(container)
+	// @ts-ignore
+	global.QualtricsMapBoxLibrary = {
+		mapRender
+	}
+})(window)
+// ;(window as any).mapRender = mapRender
 
-	const directionContainer = target.querySelectorAll('[role*=presentation]')[0] as HTMLElement
-
-	const inputHTMLElements = directionContainer.getElementsByTagName('input')
-	const labelHTMLElements = directionContainer.getElementsByTagName('label')
-
-	const inputHtmlArray = [...labelHTMLElements].map((item, index) => ({
-		label: item.children[0].textContent ? item.children[0].textContent : '',
-		htmlElement: inputHTMLElements[index],
-		elementIndex: index
-	}))
-
-	render(
-		<React.StrictMode>
-			<ThemeCustomization>
-				<MapContainer mapBoxToken={accessToken} defaultView={options?.defaultView} inputHtmlArray={inputHtmlArray} />
-			</ThemeCustomization>
-		</React.StrictMode>,
-		document.getElementById(`MapContainer${target.id}`)
-	)
-	directionContainer.style.display = 'none'
-}
-
-;(window as any).mapRender = mapRender
+/*
+<script>  jQuery.noConflict(); </script><br /><link href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" rel="stylesheet" type="text/css" /><link href="https://api.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.css" rel="stylesheet" /><script src="https://cdn.jsdelivr.net/gh/keita-makino/qualtrics-map@3.1.0/dist/bundle.js"></script>
+<script>  jQuery.noConflict(); </script><br /><link href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" rel="stylesheet" type="text/css" /><link href="https://api.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.css" rel="stylesheet" /><link href="https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.2.5/dist/style.css" rel="stylesheet" /> <script src='https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.2.5/dist/bundle.js'></script>
+ */
