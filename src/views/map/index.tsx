87d57734f34af6ec150014cf6a411596e1691f79
Diego Cancedda <diego.cancedda.dc@gmail.com>
2024-11-04 12:12:50 +0100

    :bug: fix: Aggiorna alcune configurazioni di build per risolvere alcuni errori

--- src/views/map/index.tsx
+++ src/views/map/index.tsx
@@ -68,7 +68,6 @@ const MapContainer = ({ mapBoxToken, defaultView, inputHtmlArray }: any) => {
 						</Marker>
 					))}
 
-				<NavigationControl position="top-right" />
 				<Box sx={{ position: 'absolute', bottom: 25, left: 'calc(50% - 24px)' }}>
 					<Button variant="contained" onClick={removeAllPin}>
 						Elimina pin
@@ -79,4 +78,5 @@ const MapContainer = ({ mapBoxToken, defaultView, inputHtmlArray }: any) => {
 	)
 }
 
+// <NavigationControl position="top-right" />
 export default MapContainer
