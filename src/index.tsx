7ead5d6ab4aa757e74c588bc63710def294bdab9
Diego Cancedda <diego.cancedda.dc@gmail.com>
2024-11-06 12:13:53 +0100

    :bug: fix: Aggiorna alcune configurazioni di build per risolvere alcuni errori

--- src/index.tsx
+++ src/index.tsx
@@ -47,8 +47,8 @@ function iframeRender(accessToken: any, target: HTMLElement, options: any) {
     		<link href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" rel="stylesheet" type="text/css" />
     		<link href="https://api.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.css" rel="stylesheet" />
     		
-    		<link href="https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.4.01/dist/style.css" rel="stylesheet" />
-    		<script src='https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.4.01/dist/bundle.js'></script>
+    		<link href="https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.4.02/dist/style.css" rel="stylesheet" />
+    		<script src='https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.4.02/dist/bundle.js'></script>
     		<style>
            
         </style>
@@ -80,7 +80,7 @@ function iframeRender(accessToken: any, target: HTMLElement, options: any) {
 		iframeContainer.contentWindow?.postMessage({ accessToken, options, targetId: target.id, inputHtmlArray }, '*')
 		// Nota: Sostituisci '*' con l'origine specifica per maggiore sicurezza
 	}
-	directionContainer.style.display = 'none'
+	// directionContainer.style.display = 'none'
 
 	window.addEventListener(
 		'message',
