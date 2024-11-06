3ffe26b0f5990dd5efb361548345d6ab78abf288
Diego Cancedda <diego.cancedda.dc@gmail.com>
2024-11-06 11:56:50 +0100

    :bug: fix: Aggiorna alcune configurazioni di build per risolvere alcuni errori

--- src/index.tsx
+++ src/index.tsx
@@ -47,8 +47,8 @@ function iframeRender(accessToken: any, target: HTMLElement, options: any) {
     		<link href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" rel="stylesheet" type="text/css" />
     		<link href="https://api.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.css" rel="stylesheet" />
     		
-    		<link href="https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.3.99/dist/style.css" rel="stylesheet" />
-    		<script src='https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.3.99/dist/bundle.js'></script>
+    		<link href="https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.4.0/dist/style.css" rel="stylesheet" />
+    		<script src='https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.4.0/dist/bundle.js'></script>
     		<style>
            
         </style>
@@ -61,6 +61,14 @@ function iframeRender(accessToken: any, target: HTMLElement, options: any) {
                     parent.postMessage({ action: 'resizeIframe', height }, '*');
                 }
                 // window.addEventListener('load', sendHeightToParent);
+                const resizeObserver = new ResizeObserver(entries => {
+    				for (let entry of entries) {
+        				const height = entry.contentRect.height + 1;
+        				parent.postMessage({ action: 'resizeIframe', height }, '*');
+    				}
+				});
+
+				resizeObserver.observe(document.body);
                 
         		window.addEventListener('message', function(event) {
           			const { accessToken, options, targetId, inputHtmlArray } = event.data
