405a6a3721977b18b692275bad70da7d5792cf32
Diego Cancedda <diego.cancedda.dc@gmail.com>
2024-11-06 09:46:44 +0100

    :bug: fix: Aggiorna alcune configurazioni di build per risolvere alcuni errori

--- src/index.tsx
+++ src/index.tsx
@@ -41,8 +41,8 @@ function iframeRender(accessToken: any, target: HTMLElement, options: any) {
     		<link href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" rel="stylesheet" type="text/css" />
     		<link href="https://api.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.css" rel="stylesheet" />
     		
-    		<link href="https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.3.4/dist/style.css" rel="stylesheet" />
-    		<script src='https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.3.4/dist/bundle.js'></script>
+    		<link href="https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.3.5/dist/style.css" rel="stylesheet" />
+    		<script src='https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.3.5/dist/bundle.js'></script>
         </head>
         <body>
             <div id="MapContainer${target.id}"></div>
@@ -106,11 +106,6 @@ const mapRender = (
 		}
 	}
 ) => {
-	const container = document.createElement('div')
-	container.setAttribute('id', `MapContainer${target.id}`)
-
-	target.getElementsByClassName('ChoiceStructure')[0].appendChild(container)
-
 	render(
 		<React.StrictMode>
 			<ThemeCustomization>
@@ -122,8 +117,3 @@ const mapRender = (
 }
 ;(window as any).mapRender = mapRender
 ;(window as any).iframeRender = iframeRender
-
-/*
-<script>  jQuery.noConflict(); </script><br /><link href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" rel="stylesheet" type="text/css" /><link href="https://api.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.css" rel="stylesheet" /><script src="https://cdn.jsdelivr.net/gh/keita-makino/qualtrics-map@3.1.0/dist/bundle.js"></script>
-<script>  jQuery.noConflict(); </script><br /><link href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" rel="stylesheet" type="text/css" /><link href="https://api.mapbox.com/mapbox-gl-js/v3.1.0/mapbox-gl.css" rel="stylesheet" /><link href="https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.2.6/dist/style.css" rel="stylesheet" /> <script src='https://cdn.jsdelivr.net/gh/revodigital/uni2-web-client@0.2.6/dist/bundle.js'></script>
- */
